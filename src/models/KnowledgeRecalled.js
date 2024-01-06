3a767bf72175af678933f13786ffd4294cfd635a
Christian Turner <cturnrq@amazon.com>
2024-01-06 09:22:26 -0500

    Re desigining the scope of the knowledge Recalled class
    This class should be responsible for for initializing the application, managing hooks and their states, but things that
    occur on any of the data models should occur their rather in this location. Anything pertaining to hooks likely need
    there own HookManagement class which should initialize some sort of event listener and maintain a singleton state.

--- src/models/KnowledgeRecalled.js
+++ src/models/KnowledgeRecalled.js
@@ -1,50 +1,42 @@
 
 
 // eslint-disable-next-line no-unused-vars
-export default class KnowledgeRecalled extends Application
-{
-   documentedActors = [];
-   encounteredNPCActors = [];
-   npcActors = [];
+export default class KnowledgeRecalled extends Application {
+    loadedNPCs = [];
+    NpcsInEncounters = [];
+    npcActors = [];
 
-   static _onReady(listOfFoundryActors)
-   {
-      ui.KnowledgeRecalled = new KnowledgeRecalled(listOfFoundryActors);
-   }
+    static _onReady(listOfFoundryActors) {
+        ui.KnowledgeRecalled = new KnowledgeRecalled(listOfFoundryActors);
+    }
 
-   getActors()
-   {
-      return this.documentedActors;
-   }
+    getActors() {
+        return this.loadedNPCs;
+    }
 
-   getEncounteredActors()
-   {
-      return this.encounteredNPCActors;
-   }
+    getEncounteredActors() {
+        return this.NpcsInEncounters;
+    }
 
-   addToEncounteredActorArray(NPCActor)
-   {
-      const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
-      if (!isDuplicate)
-      {
-         this.encounteredNPCActors.push(NPCActor);
-      }
-   }
+    addToEncounteredActorArray(NPCActor) {
+        const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
+        if (!isDuplicate) {
+            this.encounteredNPCActors.push(NPCActor);
+        }
+    }
 
-   addToNpcActorsArray(NPCActor)
-   {
-      // const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
-      // if (!isDuplicate)
-      // {
-      //    this.npcActors.push(NPCActor);
-      // }
-      this.npcActors.push(NPCActor);
-   }
+    addToNpcActorsArray(NPCActor) {
+        // const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
+        // if (!isDuplicate)
+        // {
+        //    this.npcActors.push(NPCActor);
+        // }
+        this.npcActors.push(NPCActor);
+    }
 
-   checkForDuplicateActor(actorId)
-   {
-      return this.documentedActors.some((npcActor) => npcActor.actorId === actorId);
-   }
+    checkForDuplicateActor(actorId) {
+        return this.documentedActors.some((npcActor) => npcActor.actorId === actorId);
+    }
 
 
 }
