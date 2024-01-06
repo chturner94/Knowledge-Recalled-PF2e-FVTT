52623bb22dd86d57b0399a2052389f8500a2360a
Christian Turner <cturnrq@amazon.com>
2024-01-06 09:18:21 -0500

    Added an EncounterModel for managing the data layer of encounters within Foundry

---
+++ src/models/EncounterModel.js
@@ -0,0 +1,16 @@
+export default class EncounterModel 
+{
+    // encounters should be created at startup, and not stored.
+    /**
+     * @class
+     *
+     * @param combat - it's expected that combat will be game.combats[0]. Likely  need to iterate through if multiple
+     * combats are available.
+     */
+    constructor(combat) 
+{
+        this.combat = combat;
+        this.NPCS = combat.combatants;
+
+    }
+}
