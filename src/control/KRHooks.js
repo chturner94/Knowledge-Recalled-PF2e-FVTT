a9e320d08e0913e5c3f1dde2cba4651e82e7aabc
Christian Turner <cturnrq@amazon.com>
2024-01-14 22:18:42 -0500

    added hook for when application closes

--- src/control/KRHooks.js
+++ src/control/KRHooks.js
@@ -1,4 +1,5 @@
 import KnowledgeRecalled from "../KnowledgeRecalled";
+import { insertKnowledgeRecalledbuttons } from "../foundryUiOverrides";
 import ViewManager from "./ViewManager";
 export default async function registerHooks() {
     Hooks.on("ready", () => {
@@ -6,10 +7,13 @@ export default async function registerHooks() {
         ViewManager._onReady();
         ui.KnowledgeRecalled.ViewManager.init();
     });
-
     Hooks.on('getSceneControlButtons', (controls) => {
         insertKnowledgeRecalledbuttons(controls);
     });
+    Hooks.on('closeApplication', (app, html) => {
+        console.log(`Here is the application`, app);
+        console.log(`Here is the html`, html);
+    })
 
 };
 
