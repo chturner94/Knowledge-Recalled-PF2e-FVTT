50879cdf7b3f0e885e487ca678ef45e89ddccfc7
Christian Turner <cturnrq@amazon.com>
2024-01-14 22:11:43 -0500

    removed console log

--- src/control/KRHooks.js
+++ src/control/KRHooks.js
@@ -2,13 +2,9 @@ import KnowledgeRecalled from "../KnowledgeRecalled";
 import ViewManager from "./ViewManager";
 export default async function registerHooks() {
     Hooks.on("ready", () => {
-        console.log("Foundry is Ready");
         KnowledgeRecalled._onReady();
-        console.log("KnowledgeRecalled - registered")
         ViewManager._onReady();
-        console.log("Knowledge Recalled - Viewmanager and AppService Registered")
         ui.KnowledgeRecalled.ViewManager.init();
-        console.log("KnowledgeRecalled - ViewManager setup")
     });
 
     Hooks.on('getSceneControlButtons', (controls) => {
